2535c9b9196a1c78818f6ebac5926132eecd58f3
Aya Ahmed <ayaahmeed963@gmail.com>
2024-05-14 13:21:16 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DisplayPage from './components/pages/DisplayPage';
+import LoginPage from './components/pages/LoginPage';
+import { UserProvider } from './components/context/Context';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+    <UserProvider>
+      <Routes>
+        <Route path="/" element={<LoginPage />}/>
+        <Route path="/userinfo" element={<DisplayPage />}/>
+      </Routes>
+    </UserProvider>
+    </BrowserRouter>
+    </>
   );
 }
 
